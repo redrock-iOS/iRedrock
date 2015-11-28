220fa892a0963818f533f84db216a0e7c8a0e67d
RainyTunes <cquptrainytunes@gmail.com>
2015-11-29 07:28:00 +0800

    5th Main Lesson

---
+++ 第五次大课/Demo_Gesture/Demo_Gesture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo_Gesture
+//
+//  Created by RainyTunes on 11/29/15.
+//  Copyright © 2015 We.Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
